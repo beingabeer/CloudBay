d23571b0e309d7b1c771dd570916e0b4728d6f7e
abeer <abeer.sewak@gmail.com>
2020-01-17 16:35:49 -0600

    Update home-page.html

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import item_list
+from . import views
 
 app_name = "core"
 
 urlpatterns = [
-    path("", item_list, name="item-list"),
+    path("", views.item_list, name="item-list"),
+    # path("home-page/", views.home, name="item-list"),
 ]
