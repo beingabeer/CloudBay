3430114879703c921f1cd732e45e650e644567e7
abeer <abeer.sewak@gmail.com>
2020-01-17 21:30:37 -0600

    Added logic to slugify each item uniquely

--- src/core/models.py
+++ src/core/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.conf import settings
 from django.shortcuts import reverse
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
+
 
 CATEGORY_CHOICES = (("S", "Shirt"), ("SW", "Sport wear"), ("OW", "Outwear"))
 
@@ -12,7 +15,7 @@ class Item(models.Model):
     price = models.FloatField()
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -37,3 +40,23 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    queryset = Item.objects.filter(slug=slug).order_by("-id")
+    exists = queryset.exists()
+    if exists:
+        new_slug = f"{slug}-{queryset.first().id}"
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+
+pre_save.connect(pre_save_post_receiver, sender=Item)
