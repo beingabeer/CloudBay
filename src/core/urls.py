3ab1fcccaf1127d311100d5991804ddf0c97bba1
abeer <abeer.sewak@gmail.com>
2020-01-17 18:53:39 -0600

    Template inheritance from base.html

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,5 +5,6 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.item_list, name="item-list"),
-    # path("home-page/", views.home, name="item-list"),
+    path("product/", views.product, name="product-page"),
+    path("checkout/", views.checkout, name="checkout-page"),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -7,3 +7,11 @@ def item_list(request):
     context = {"items": Item.objects.all()}
     return render(request, "home-page.html", context)
 
+
+def checkout(request):
+    return render(request, "checkout-page.html")
+
+
+def product(request):
+    return render(request, "product-page.html")
+
